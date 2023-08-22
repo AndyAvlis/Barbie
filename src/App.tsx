bc6e259d756b34096b76713102a5e3a36dc9746a
AndyAvlis <andressa.miranda2@estudante.ifms.edu.br>
2023-08-22 08:05:55 -0400

    iniciando a programação do footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
-import Footer from './componentes/footer/Footer'
+// import Footer from './componentes/footer/Footer'
 import Header from './componentes/header/Header'
-
+import Footer1 from './componentes/footer1/Footer1'
 
 function App() {
   return (
-    <><Header /><Footer /></>
+    <>
+      <Header />
+      {/* <Footer /> */}
+      <Footer1 />
+    </>
   )
 }
 export default App
---
+++ src/componentes/footer1/Footer1.tsx
@@ -0,0 +1,12 @@
+import './Footer1.css'
+import Logo from '../../assets/Logo.webp'
+
+function Footer1() {
+    return(
+        <footer>
+            <img src={Logo} alt="" />
+        </footer>   
+    )
+}
+
+export default Footer1
