7f1f3230e8eadfe5cc52db72f92a0b0407a734aa
AndyAvlis <andressa.miranda2@estudante.ifms.edu.br>
2023-11-23 09:28:40 -0400

    feito

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,31 +1,41 @@
 import './Main.css'
 import Filme from "../filme/Filme"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 type FilmesType = {
     id: number,
     titulo: string,
-    sinopse: string,
+    descricao: string,
     imagem: string
 }
 
 function Main(){
+
+    const url = "http://localhost:3000/filmes"    
+    const [filmes,setDados] = useState<FilmesType[]>([])
+    
+     useEffect(()=>{
+         fetch(url, {method:"GET"})
+         .then((resposta)=>resposta.json())
+         .then((json)=>setDados(json))
+     },[])  
+
     //Hook
     const [texto,setTexto] = useState("")
 
-    const filmes:FilmesType[] = [
-        {
-            id: 1,
-            titulo:"Hazbin Hotel",
-            sinopse:'A série segue Charlie Magne (Jill Harris), a princesa do Inferno, uma demônio carismática e herdeira do trono, enquanto se empenha em realizar seu sonho aparentemente impossível de abrir um hotel chamado "Happy Hotel", que visa reabilitar pecadores.',
-            imagem:"/HAZBINHOTEL.jpg"
-        },
-        {
-            id: 2,
-            titulo:"Barbie",
-            sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
-            imagem:"/Barbie.jpg"
-        }
-]
+//     const filmes:FilmesType[] = [
+//         {
+//             id: 1,
+//             titulo:"Hazbin Hotel",
+//             sinopse:'A série segue Charlie Magne (Jill Harris), a princesa do Inferno, uma demônio carismática e herdeira do trono, enquanto se empenha em realizar seu sonho aparentemente impossível de abrir um hotel chamado "Happy Hotel", que visa reabilitar pecadores.',
+//             imagem:"/HAZBINHOTEL.jpg"
+//         },
+//         {
+//             id: 2,
+//             titulo:"Barbie",
+//             sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
+//             imagem:"/Barbie.jpg"
+//         }
+// ]
     //A função recebe um atributo chamado "event"
     function mudaTexto(e:React.ChangeEvent<HTMLInputElement>){
         console.log(e.target.value)
@@ -43,7 +53,7 @@ function Main(){
         </div>
         
         <main className="content-main">
-        { filmes.filter((filme)=>filme.titulo.toLocaleLowerCase().includes(texto)).map((filme) => <Filme key = { filme.id } titulo={filme.titulo} sinopse={filme.sinopse} imagem={filme.imagem}/>) }        </main>
+        { filmes.filter((filme)=>filme.titulo.toLocaleLowerCase().includes(texto)).map((filme) => <Filme key = { filme.id } titulo={filme.titulo} sinopse={filme.descricao} imagem={filme.imagem}/>) }        </main>
     </>
     )
 }
